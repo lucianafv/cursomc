027b328952489f2bc363c1a8a4a0ee205a40031e
Luciana Viana <lufonteles@gmail.com>
2019-05-07 00:32:39 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/lucianafv/cursomc/CursomcApplication.java
+++ src/main/java/com/lucianafv/cursomc/CursomcApplication.java
@@ -72,7 +72,7 @@ public class CursomcApplication implements CommandLineRunner {
 		Categoria cat2 = new Categoria (null, "Escritório");
 		
 		Produto p1 = new Produto(null,"Computador",2000.00);
-		Produto p2 = new Produto(null, "Ic3mpressora",800.00);
+		Produto p2 = new Produto(null, "Impressora",800.00);
 		Produto p3 = new Produto(null, "Mouse",80.00);
 		
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
---
+++ src/main/java/com/lucianafv/cursomc/services/PedidoService.java
@@ -0,0 +1,25 @@
+package com.lucianafv.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lucianafv.cursomc.domain.Pedido;
+import com.lucianafv.cursomc.repositories.PedidoRepository;
+import com.lucianafv.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	/* automaticamente instanciada pelo spring seja pelo mecanismo de injeção de dependência seja pela inversão de controle = autowired */
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+
+	}
+}
